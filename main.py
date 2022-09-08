c3af585707155a3c8ebde61bfbce62a10a4ac68b
Eddie <85015209+xXEddieXxx@users.noreply.github.com>
2022-09-08 15:09:25 +0200

    initial commit

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import time
 class Main:
     print("20 min Afk Script")
 
-    for timeleft in range(20):
+    for i in range(20):
         mouse.move(100, 100, 100, 0.2)
         mouse.move(50, 50, 50, 0.2)
         time.sleep(60)
 
-        print(timeleft, " minutes over")
+        print(i, " minutes over")
 
     input("Time is Over, press enter to exit")
